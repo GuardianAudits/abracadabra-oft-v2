fef78f06022d075858e880b7d361cb26034a5c71
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2024-12-14 12:50:55 -0500

    updated wired config

--- layerzero.config.ts
+++ layerzero.config.ts
@@ -1,5 +1,7 @@
-import { EndpointId } from '@layerzerolabs/lz-definitions'
-import type { OAppOmniGraphHardhat, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
+import { ExecutorOptionType } from "@layerzerolabs/lz-v2-utilities";
+import { OAppEnforcedOption, OmniPointHardhat } from "@layerzerolabs/toolbox-hardhat";
+import { EndpointId } from "@layerzerolabs/lz-definitions";
+import { generateConnectionsConfig } from "@layerzerolabs/metadata-tools";
 
 const mainnetContract: OmniPointHardhat = {
     eid: EndpointId.ETHEREUM_V2_MAINNET,
@@ -11,25 +13,39 @@ const arbitrumContract: OmniPointHardhat = {
     contractName: 'SpellOFTUpgradeable',
 }
 
-const config: OAppOmniGraphHardhat = {
-    contracts: [
-        {
-            contract: mainnetContract,
-        },
-        {
-            contract: arbitrumContract,
-        }
-    ],
-    connections: [
-        {
-            from: mainnetContract,
-            to: arbitrumContract,
-        },
-        {
-            from: arbitrumContract,
-            to: mainnetContract,
-        }
-    ],
-}
+const EVM_ENFORCED_OPTIONS: OAppEnforcedOption[] = [
+    {
+        msgType: 1,
+        optionType: ExecutorOptionType.LZ_RECEIVE,
+        gas: 80000,
+        value: 0,
+    },
+    {
+        msgType: 2,
+        optionType: ExecutorOptionType.LZ_RECEIVE,
+        gas: 80000,
+        value: 0,
+    },
+    {
+        msgType: 2,
+        optionType: ExecutorOptionType.COMPOSE,
+        index: 0,
+        gas: 80000,
+        value: 0,
+    },
+];
 
-export default config
+export default async function () {
+    // [srcContract, dstContract, [requiredDVNs, [optionalDVNs, threshold]], [srcToDstConfirmations, dstToSrcConfirmations]], [enforcedOptionsSrcToDst, enforcedOptionsDstToSrc]
+    const connections = await generateConnectionsConfig([
+        [mainnetContract, arbitrumContract, [['LayerZero Labs'], []], [1, 1], [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS]],
+    ]);
+
+    return {
+        contracts: [
+            { contract: mainnetContract },
+            { contract: arbitrumContract },
+        ],
+        connections
+    }
+}
