f95e0cc96b389b3afa1bbdd9fbae34a21e453630
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2024-12-15 11:11:54 -0500

    chore: forge init

---
+++ contracts/mocks/MockAggregator.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.22;
+
+contract MockAggregator {
+    int256 private answer;
+    uint8 public constant decimals = 8;
+
+    function setAnswer(int256 _answer) external {
+        answer = _answer;
+    }
+
+    function latestAnswer() external view returns (int256) {
+        return answer;
+    }
+} 
