81e6134c4cd592ccbf6e955d2d4273dd931f3d3e
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2025-01-15 10:49:44 -0500

    removed useless configuration code

--- layerzero.config.ts
+++ layerzero.config.ts
@@ -60,31 +60,12 @@ const EVM_ENFORCED_OPTIONS: OAppEnforcedOption[] = [
     },
 ];
 
-const setConfirmations = (config: OmniEdgeHardhat<OAppEdgeConfig>[], fromEid: number, toEid: number, sendConfirmations: number, receiveConfirmations: number) => {
-    config.forEach(edge => {
-        if (edge.from.eid === fromEid && edge.to.eid === toEid) {
-            // Update send config confirmations
-            if (edge.config?.sendConfig?.ulnConfig) {
-                edge.config.sendConfig.ulnConfig.confirmations = BigInt(sendConfirmations.toString());
-            }
-
-            // Update receive config confirmations on the destination
-            if (edge.config?.receiveConfig?.ulnConfig) {
-                edge.config.receiveConfig.ulnConfig.confirmations = BigInt(receiveConfirmations.toString());
-            }
-        }
-    });
-}
-
 export default async function () {
     // [srcContract, dstContract, [requiredDVNs, [optionalDVNs, threshold]], [srcToDstConfirmations, dstToSrcConfirmations]], [enforcedOptionsSrcToDst, enforcedOptionsDstToSrc]
     const connections = await generateConnectionsConfig([
-        [spellEthereumContract, spellArbitrumContract, [['LayerZero Labs', 'MIM'], []], [1, 1], [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS]],
-        [bSpellEthereumContract, bSpellArbitrumContract, [['LayerZero Labs', 'MIM'], []], [1, 1], [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS]],
+        [spellEthereumContract, spellArbitrumContract, [['LayerZero Labs', 'MIM'], []], [15, 20], [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS]],
+        [bSpellEthereumContract, bSpellArbitrumContract, [['LayerZero Labs', 'MIM'], []], [15, 20], [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS]],
     ]) as OmniEdgeHardhat<OAppEdgeConfig>[];
-
-    setConfirmations(connections, EndpointId.ETHEREUM_V2_MAINNET, EndpointId.ARBITRUM_V2_MAINNET, 15, 20);
-    setConfirmations(connections, EndpointId.ARBITRUM_V2_MAINNET, EndpointId.ETHEREUM_V2_MAINNET, 20, 15);
 
     // Prints generated connections
     //console.log(JSON.stringify(connections, (_, value) => typeof value === 'bigint' ? value.toString() : value, 2));
